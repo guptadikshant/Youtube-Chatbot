425ad33ac4761620ef4688d2acd64c6018330f9e
Dikshant Gupta <guptadikshant99@gmail.com>
2024-08-19 16:18:57 +0530

    code to appear Q&A UI only after Video Transcription completed

--- main.py
+++ main.py
@@ -17,52 +17,62 @@ def main() -> None:
     # Initialize the video_links dictionary in session state
     if "video_links" not in st.session_state:
         st.session_state.video_links = {}
-    
+
     if "video_file_path" not in st.session_state:
         st.session_state.video_file_path = None
-    
-    video_link = st.text_input("Upload your videos here.....")
 
-    saved_videos_dir = "saved_videos_dir"
-    
-    if video_link:
-        with st.spinner("Saving Video Transcript. This will take few mins......."):
-            # Check if the video link is new or already processed
-            if video_link not in st.session_state.video_links:
-                # Generate a new UUID for the new video link
-                new_uuid = uuid.uuid4()
-                st.session_state.video_links[video_link] = new_uuid
+    if "transcription_done" not in st.session_state:
+        st.session_state.transcription_done = False
 
-                # Create a new subdirectory with the generated UUID
-                st.session_state.video_file_path = os.path.join(saved_videos_dir, str(new_uuid))
-                os.makedirs(st.session_state.video_file_path, exist_ok=True)
+    with st.container():
+        if not st.session_state.transcription_done:
+            video_link = st.text_input("Upload your videos here.....")
 
-                download_youtube_video_transcript(
-                    video_link=video_link,
-                    path=os.path.abspath(st.session_state.video_file_path)
-                )
-                st.success("Video transcript saved successfully!!!")
-            else:
-                # Use the existing UUID for the video link
-                st.session_state.video_file_path = os.path.join(saved_videos_dir, str(st.session_state.video_links[video_link]))
-                st.write(f"Using existing directory: {st.session_state.video_file_path}")
-    
-    user_question = st.text_input("Enter your questions here......")
+            saved_videos_dir = "saved_videos_dir"
 
-    generate_response = st.button("Generate Response")
+            if video_link:
+                with st.spinner("Saving Video Transcript. This will take few mins......."):
+                    # Check if the video link is new or already processed
+                    if video_link not in st.session_state.video_links:
+                        # Generate a new UUID for the new video link
+                        new_uuid = uuid.uuid4()
+                        st.session_state.video_links[video_link] = new_uuid
 
-    if user_question and generate_response:
-        with st.spinner("Generating Response....."):
-            with open(os.path.join(st.session_state.video_file_path, "transcript.txt"), "r") as file:
-                transcript_data = file.read()
+                        # Create a new subdirectory with the generated UUID
+                        st.session_state.video_file_path = os.path.join(saved_videos_dir, str(new_uuid))
+                        os.makedirs(st.session_state.video_file_path, exist_ok=True)
 
-            final_output = get_model_output(
-                video_transcript=transcript_data,
-                question=user_question
-            )
+                        download_youtube_video_transcript(
+                            video_link=video_link,
+                            path=os.path.abspath(st.session_state.video_file_path)
+                        )
+                        st.success("Video transcript saved successfully!!!")
+                        st.session_state.transcription_done = True
+                    else:
+                        # Use the existing UUID for the video link
+                        st.session_state.video_file_path = os.path.join(saved_videos_dir,
+                                                                        str(st.session_state.video_links[video_link]))
+                        st.write(f"Using existing directory: {st.session_state.video_file_path}")
+                        st.session_state.transcription_done = True
 
-            if final_output:
-                st.write(final_output)
+    if st.session_state.transcription_done:
+        with st.container():
+            user_question = st.text_input("Enter your questions here......")
+
+            generate_response = st.button("Generate Response")
+
+            if user_question and generate_response:
+                with st.spinner("Generating Response....."):
+                    with open(os.path.join(st.session_state.video_file_path, "transcript.txt"), "r") as file:
+                        transcript_data = file.read()
+
+                    final_output = get_model_output(
+                        video_transcript=transcript_data,
+                        question=user_question
+                    )
+
+                    if final_output:
+                        st.write(final_output)
 
 
 if __name__ == '__main__':
